13625704c3a9f844136d1b5e0f49b752ef25dea8
James FL <jlacandula@gmail.com>
2021-03-06 07:34:43 +0800

    modularize default layout

---
+++ components/core/NavDrawer.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant="miniVariant"
+    :clipped="clipped"
+    fixed
+    app
+  >
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        icon: 'mdi-apps',
+        title: 'Dashboard',
+        to: '/',
+      },
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire',
+        to: '/inspire',
+      },
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Admin',
+        to: '/admin',
+      },
+    ],
+  }),
+}
+</script>
